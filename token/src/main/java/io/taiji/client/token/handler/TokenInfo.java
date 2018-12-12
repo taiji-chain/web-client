d19c1cd4998abced05ed0dc2135ba03910d185e3
Steve Hu <stevehu@gmail.com>
2018-12-11 23:11:29 -0500

    fixes #19 add a branch to get all tokens if address or symbol is missed

--- token/src/main/java/io/taiji/client/token/handler/TokenInfo.java
+++ token/src/main/java/io/taiji/client/token/handler/TokenInfo.java
@@ -10,6 +10,7 @@ import com.networknt.utility.NioUtils;
 import com.networknt.rpc.Handler;
 import com.networknt.rpc.router.ServiceHandler;
 import java.nio.ByteBuffer;
+import java.util.List;
 import java.util.Map;
 
 import io.undertow.server.HttpServerExchange;
@@ -25,23 +26,34 @@ public class TokenInfo implements Handler {
     public ByteBuffer handle(HttpServerExchange exchange, Object input)  {
         Map<String, String> map = (Map<String, String>)input;
         String tokenAddressOrSymbol = map.get("tokenAddressOrSymbol");
-        if (!Keys.validateToAddress(tokenAddressOrSymbol)) {
-            // check if it is a symbol by getting the token info by symbol.
-            Result<Map<String, Object>> tokenInfoResult = TaijiClient.getTokenInfoBySymbol(tokenAddressOrSymbol);
-            if(tokenInfoResult.isSuccess()) {
-                Map<String, Object> tokenInfo = tokenInfoResult.getResult();
-                return NioUtils.toByteBuffer(JsonMapper.toJson(tokenInfo));
+        if(tokenAddressOrSymbol != null) {
+            if (!Keys.validateToAddress(tokenAddressOrSymbol)) {
+                // check if it is a symbol by getting the token info by symbol.
+                Result<Map<String, Object>> tokenInfoResult = TaijiClient.getTokenInfoBySymbol(tokenAddressOrSymbol);
+                if(tokenInfoResult.isSuccess()) {
+                    Map<String, Object> tokenInfo = tokenInfoResult.getResult();
+                    return NioUtils.toByteBuffer(JsonMapper.toJson(tokenInfo));
+                } else {
+                    return NioUtils.toByteBuffer(getStatus(exchange, tokenInfoResult.getError()));
+                }
             } else {
-                return NioUtils.toByteBuffer(getStatus(exchange, tokenInfoResult.getError()));
+                // valid address and this must be tokenAddress
+                Result<Map<String, Object>> tokenInfoResult = TaijiClient.getTokenInfoByAddress(tokenAddressOrSymbol);
+                if(tokenInfoResult.isSuccess()) {
+                    Map<String, Object> tokenInfo = tokenInfoResult.getResult();
+                    return NioUtils.toByteBuffer(JsonMapper.toJson(tokenInfo));
+                } else {
+                    return NioUtils.toByteBuffer(getStatus(exchange, tokenInfoResult.getError()));
+                }
             }
         } else {
-            // valid address and this must be tokenAddress
-            Result<Map<String, Object>> tokenInfoResult = TaijiClient.getTokenInfoByAddress(tokenAddressOrSymbol);
-            if(tokenInfoResult.isSuccess()) {
-                Map<String, Object> tokenInfo = tokenInfoResult.getResult();
-                return NioUtils.toByteBuffer(JsonMapper.toJson(tokenInfo));
+            // there is no tokenAddressOrSymbol, get all tokens
+            Result<List<Map<String, Object>>> tokenInfosResult = TaijiClient.getTokenInfos(0, 10);
+            if(tokenInfosResult.isSuccess()) {
+                List<Map<String, Object>> tokenInfos = tokenInfosResult.getResult();
+                return NioUtils.toByteBuffer(JsonMapper.toJson(tokenInfos));
             } else {
-                return NioUtils.toByteBuffer(getStatus(exchange, tokenInfoResult.getError()));
+                return NioUtils.toByteBuffer(getStatus(exchange, tokenInfosResult.getError()));
             }
         }
     }
