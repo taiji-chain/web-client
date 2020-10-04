48d17426630b7a7f3db9dc9ef49768fa86a072e6
Steve Hu <stevehu@gmail.com>
2020-10-03 21:25:04 -0400

    fixes #57 add pageNumber and pageSize for current transaction

--- currency/src/main/java/io/taiji/client/currency/handler/GetTransaction.java
+++ currency/src/main/java/io/taiji/client/currency/handler/GetTransaction.java
@@ -1,15 +1,12 @@
 
 package io.taiji.client.currency.handler;
 
-import com.networknt.config.JsonMapper;
 import com.networknt.monad.Result;
 import com.networknt.taiji.client.TaijiClient;
-import com.networknt.taiji.crypto.SignedLedgerEntry;
 import com.networknt.utility.NioUtils;
 import com.networknt.rpc.Handler;
 import com.networknt.rpc.router.ServiceHandler;
 import java.nio.ByteBuffer;
-import java.util.List;
 import java.util.Map;
 
 import io.undertow.server.HttpServerExchange;
@@ -18,14 +15,15 @@ import io.undertow.server.HttpServerExchange;
 public class GetTransaction implements Handler {
     @Override
     public ByteBuffer handle(HttpServerExchange exchange, Object input)  {
-        Map<String, String> map = (Map<String, String>)input;
-        String address = map.get("address");
-        String currency = map.get("currency");
+        Map<String, Object> map = (Map<String, Object>)input;
+        String address = (String)map.get("address");
+        String currency = (String)map.get("currency");
+        int pageNumber = (Integer)map.get("pageNumber");
+        int pageSize = (Integer)map.get("pageSize");
         // otherwise, get it from the chain-reader service but don't put it into the cache as it is used for rate limiting.
-        Result<List<SignedLedgerEntry>> result = TaijiClient.getTransaction(address, currency);
+        Result<String> result = TaijiClient.getTransaction(address, currency, (pageNumber -1) * pageSize , pageSize);
         if(result.isSuccess()) {
-            List<SignedLedgerEntry> entries = result.getResult();
-            return NioUtils.toByteBuffer(JsonMapper.toJson(entries));
+            return NioUtils.toByteBuffer(result.getResult());
         } else {
             return NioUtils.toByteBuffer(getStatus(exchange, result.getError()));
         }
